71fae97a61211e7cf4c7194a926fb708f72b50df
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-14 05:30:21 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,9 +27,9 @@ class String
   end
 
   def count_sentences
-    sentence = self.split(".")
+    
     self.squeeze(" ") do |sentence|
-     
+      sentence = self.split(".")
      end
   end
   
